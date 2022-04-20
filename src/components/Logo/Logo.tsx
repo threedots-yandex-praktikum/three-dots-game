5b12f1402c49fadd6dbe7b1ac099f3b507f1a6d1
natalia127 <ndorogan@bk.ru>
2022-04-20 17:36:49 +0300

    edit size dot

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import './style.scss';
 import {TLogo} from './types'
 
 export const Logo: FC<TLogo> = (props: TLogo) => {
-  const size = props.size || 's';
+  const size = props.size || 'm';
   const classDot = `dots__item dot dot--${size}`
   return (
     <div className="dots">
