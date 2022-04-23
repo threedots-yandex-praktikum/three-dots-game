d66a945d58fbb02456fbc32bf665a93e7c8def7a
Maxim Balashov <79164211428@yandex.ru>
2022-04-23 14:32:07 +0300

    leadboard done

---
+++ src/constants/leaderBoard.ts
@@ -0,0 +1,18 @@
+export enum BALL_SIZE {
+  LARGE = "40px",
+  MEDIUM = "30px",
+  SMALL = "20px",
+}
+
+export const colors = [
+  "#FC8181",
+  "#ED8936",
+  "#FEFCBF",
+  "#48BB78",
+  "#38B2AC",
+  "#4299E1",
+  "#0BC5EA",
+  "#9F7AEA",
+  "#ED64A6",
+  "#63171B",
+];
--- src/pages/LeaderBoard/LeaderBoard.tsx
+++ src/pages/LeaderBoard/LeaderBoard.tsx
@@ -1,19 +1,137 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, } from 'react';
 import './style.scss';
+import { Box, Container, Divider, Flex, Heading, Text } from '@chakra-ui/react';
+import { LeaderBoardProps } from './types';
+import { Background } from 'components/Background/Background';
+import { BALL_SIZE, colors } from 'constants/leaderBoard';
 
 
-type LeaderBoardProps = Record<string, unknown>;
+const mockData = [
+  { id: 1, userName: 'user1', score: 20 },
+  { id: 2, userName: 'user2', score: 20 },
+  { id: 3, userName: 'user3', score: 20 },
+  { id: 4, userName: 'user4', score: 40 },
+  { id: 5, userName: 'user5', score: 50 },
+  { id: 6, userName: 'user6', score: 10 },
+  { id: 7, userName: 'userwerwerwerwer 2342432342342342347', score: 3 },
+
+]
 
 
 export const LeaderBoard: FC<LeaderBoardProps> = () => {
 
+  useEffect(() => {
+    // лоадер ON
+    // запрос данных
+    // лоадер OFF
+  })
+
   return (
-    <div>
-      таблица рекордов
-    </div>
+    <Background>
+      <Container
+        centerContent={true}
+        maxW="100%"
+      >
+        {_renderTittle()}
+        {_renderList()}
+      </Container>
+    </Background>
   )
 }
 
-LeaderBoard.propTypes = {
+const _chooseSize = (index: number): BALL_SIZE => {
+  if (index < 3) {
+    return BALL_SIZE.LARGE
+  } else if (index < 6) {
+    return BALL_SIZE.MEDIUM
+  } else {
+    return BALL_SIZE.SMALL
+  }
+}
 
+const _renderHead = () => {
+  return (
+    <React.Fragment>
+      <Flex w="100%" h="40px" px="10px" alignItems="center">
+        <Box w="40px">
+        </Box>
+        <Box flexGrow={1} ml="10px">
+          <Text fontSize="lg" fontWeight="bold">Игрок</Text>
+        </Box>
+        <Box >
+          <Text fontSize="lg" fontWeight="bold"> Очки</Text>
+        </Box>
+      </Flex>
+      <Divider
+        w="90%"
+        orientation='horizontal'
+        borderBottomColor="black"
+        border="2px"
+        my="2px"
+      />
+    </React.Fragment>
+
+  )
+}
+
+const _getRandomColor = (colorArr: Array<string>) => {
+  return colorArr[Math.floor(Math.random() * colorArr.length)]
+}
+
+const _renderList = () => {
+  return (
+    <Flex
+      bg="white"
+      margin={50}
+      maxW="500px"
+      alignItems="center"
+      boxShadow="dark-lg"
+      direction='column'
+      w="50%"
+    >
+      {_renderHead()}
+      {mockData
+        //TODO реализовать в utils свою сортировку (по заданию)
+        .sort((a, b) => b.score - a.score)
+        .map((row, index) => {
+          return (
+            <React.Fragment key={row.id}>
+              <Flex w="100%" h="40px" px="10px" alignItems="center">
+                <Flex w="40px" alignItems="center" justifyContent="center">
+                  <Box
+                    borderRadius="50%"
+                    bgColor={_getRandomColor(colors)}
+                    w={_chooseSize(index)}
+                    h={_chooseSize(index)}
+                  />
+                </Flex>
+                <Box flexGrow={1} ml="10px">
+                  {row.userName}
+                </Box>
+                <Box >
+                  {row.score}
+                </Box>
+              </Flex>
+              {index === mockData.length - 1
+                ? null
+                : <Divider
+                  w="90%"
+                  orientation='horizontal'
+                  borderBottomColor="black"
+                  border="2px"
+                  my="2px"
+                />}
+            </ React.Fragment>
+          )
+        })}
+    </Flex>
+  );
 };
+
+const _renderTittle = () => {
+  return (
+    <Heading>
+      Таблица рекордов
+    </Heading>)
+
+}
