d36da15d5f24414025765dad0e0a6bdfa5c44967
natalia127 <ndorogan@bk.ru>
2022-04-25 12:08:53 +0300

    fix

--- src/pages/GamePlay/GamePlay.tsx
+++ src/pages/GamePlay/GamePlay.tsx
@@ -1,62 +1,55 @@
-import React, { FC, useRef, useEffect } from 'react';
-import './style.scss';
-import {random, getRadians} from './utils'
-import { TDots } from './types'
+import React, { FC, useRef, useEffect } from "react";
+import "./style.scss";
+import { random, getRadians } from "./utils";
+import { TDots } from "./types";
 
 type GamePlayProps = Record<string, unknown>;
 
-const colorsDot = [
-  '#FF4903', '#4378B', '#00E691', '#8157D1', '#000'
-];
-const countColor = colorsDot.length
+const colorsDot = ["#FF4903", "#4378B", "#00E691", "#8157D1", "#000"];
+const countColor = colorsDot.length;
 const sizeCanvas = 1000;
 const minRadiusDot = 5;
 const maxRadiusDot = 40;
-const dots: TDots[] = []
-const radians = getRadians(360)
-const initStartGame = () => {
+const dots: TDots[] = [];
+const radians = getRadians(360);
+const initDots = () => {
   for (let index = 0; index < 10; index++) {
-    const radius = random(minRadiusDot, maxRadiusDot)
-    const xyMax = sizeCanvas - radius
-    const xyMin = 0 + radius
+    const radius = random(minRadiusDot, maxRadiusDot);
+    const xyMax = sizeCanvas - radius;
+    const xyMin = 0 + radius;
     const dot = {
       r: radius,
       x: random(xyMin, xyMax),
       y: random(xyMin, xyMax),
-      color: colorsDot[random(0, countColor) - 1]
-    }
-    dots.push(dot)
-    
+      color: colorsDot[random(0, countColor) - 1],
+    };
+    dots.push(dot);
   }
 };
 
 export const GamePlay: FC<GamePlayProps> = () => {
   const ref = useRef(null);
-  initStartGame();
-  
+  initDots();
+
   useEffect(() => {
     if (ref?.current) {
-      const ctx = (ref.current as HTMLCanvasElement).getContext('2d');
+      const ctx = (ref.current as HTMLCanvasElement).getContext("2d");
       if (ctx) {
         dots.forEach((dot) => {
           ctx.beginPath();
           ctx.arc(dot.x, dot.y, dot.r, 0, radians);
           ctx.fillStyle = dot.color;
           ctx.fill();
-        })
-
+        });
       }
     }
-
   }, []);
-  
+
   return (
-    <div className='playing-field'>
-      <canvas ref={ref} width={sizeCanvas} height={sizeCanvas}/>
+    <div className="playing-field">
+      <canvas ref={ref} width={sizeCanvas} height={sizeCanvas} />
     </div>
-  )
-}
-
-GamePlay.propTypes = {
-
+  );
 };
+
+GamePlay.propTypes = {};
