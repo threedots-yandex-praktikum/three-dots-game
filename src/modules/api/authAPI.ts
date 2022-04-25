7564d0c78a9db260d877e2c882153d544a87a02f
Leonid Sergeev <sls@bfg.ai>
2022-04-26 00:11:27 +0400

    #6  - из проекта первого модуля перенесена логика выполнения запросов  - корректировки на использование fetch API  - перенесены апи для авторизации профиля

---
+++ src/modules/api/authAPI.ts
@@ -0,0 +1,57 @@
+import { HTTPTransport } from './httpTransport/httpTransport';
+import { AUTH_API_ENDPOINTS, DEFAULT_POST_REQUEST_HEADERS, YANDEX_API_HOST } from './httpTransport/constants';
+
+
+export type TSignUpData = {
+  first_name: 'string',
+  second_name: 'string',
+  login: 'string',
+  email: 'string',
+  password: 'string',
+  phone: 'string'
+};
+
+export type TSignInData = {
+  login: 'string',
+  password: 'string',
+}
+
+type TSignUpResponse = {
+  id: number,
+}
+
+class AuthAPIClass {
+  authHTTPTransportInstance: HTTPTransport;
+
+  constructor() {
+    this.authHTTPTransportInstance = new HTTPTransport(YANDEX_API_HOST);
+  }
+
+  async signUp(data: TSignUpData): Promise<TSignUpResponse> {
+    const response = await this.authHTTPTransportInstance.post(
+      AUTH_API_ENDPOINTS.SIGN_UP,
+      {
+        data,
+        headers: DEFAULT_POST_REQUEST_HEADERS,
+      },
+    );
+
+    return response as TSignUpResponse;
+  }
+
+  async signIn(data: TSignInData) {
+    return await this.authHTTPTransportInstance.post(
+      AUTH_API_ENDPOINTS.SIGN_IN,
+      {
+        data,
+        headers: DEFAULT_POST_REQUEST_HEADERS,
+      },
+    );
+  }
+
+  async logOut() {
+    return await this.authHTTPTransportInstance.post(AUTH_API_ENDPOINTS.LOG_OUT);
+  }
+}
+
+export const AuthAPI = new AuthAPIClass();
---
+++ src/modules/api/httpTransport/httpTransport.ts
@@ -0,0 +1,133 @@
+import {
+  makeHTTPRequestResponse,
+  THandleCommonCodesParams,
+  THttpRequestOptions,
+  THttpRequestOptionsWithoutMethod
+} from "modules/api/httpTransport/types";
+import {HTTP_METHOD, HTTP_REQUEST_STATUS} from "./constants";
+
+
+/* eslint-disable no-console */
+export class HTTPTransport {
+  host: string;
+  defaultOptions: THttpRequestOptions;
+
+  constructor(host: string, defaultOptions?: THttpRequestOptions) {
+    this.host = host;
+    this.defaultOptions = defaultOptions || {} as THttpRequestOptions;
+  }
+
+  public get(url: string, options: THttpRequestOptionsWithoutMethod = {}): Promise<unknown> {
+    return this.makeHTTPRequest(url, { ...options, ...this.defaultOptions, method: HTTP_METHOD.GET })
+      .then(response => HTTPTransport.handleCommonCodes(response as THandleCommonCodesParams));
+  }
+
+  public post(url: string, options: THttpRequestOptionsWithoutMethod = {}): Promise<unknown> {
+    return this.makeHTTPRequest(url, { ...options, ...this.defaultOptions, method: HTTP_METHOD.POST })
+      .then(response => HTTPTransport.handleCommonCodes(response as THandleCommonCodesParams));
+  }
+
+  public put(url: string, options: THttpRequestOptionsWithoutMethod = {}): Promise<unknown> {
+    return this.makeHTTPRequest(url, { ...options, ...this.defaultOptions, method: HTTP_METHOD.PUT })
+      .then(response => HTTPTransport.handleCommonCodes(response as THandleCommonCodesParams));
+  }
+
+  public delete(url: string, options: THttpRequestOptionsWithoutMethod = {}): Promise<unknown> {
+    return this.makeHTTPRequest(url, { ...options, ...this.defaultOptions, method: HTTP_METHOD.DELETE })
+      .then(response => HTTPTransport.handleCommonCodes(response as THandleCommonCodesParams));
+  }
+
+  public queryStringify(data?: Record<string, unknown>): string {
+    if(!data || Object.keys(data).length === 0) {
+      return '';
+    }
+
+    return Object
+      .keys(data)
+      .reduce(
+        (acc, param, index) => {
+          const value = data[param];
+          const paramBlock = [param, value].join('=');
+
+          acc = index === Object.keys(data).length -1 ?
+            acc + paramBlock :
+            acc + paramBlock + '&';
+          return acc;
+        },
+        '?',
+      );
+  }
+
+  private static handleCommonCodes({ status, response }: THandleCommonCodesParams) {
+    switch(status) {
+      case HTTP_REQUEST_STATUS.SUCCESS: {
+        return Promise.resolve(response);
+      }
+      case HTTP_REQUEST_STATUS.UNAUTHORIZED: {
+        console.log('Неизвестный пользователь');
+        return Promise.reject({ reason: 'Неизвестный пользователь', response, status });
+      }
+      case HTTP_REQUEST_STATUS.FORBIDDEN: {
+        console.log('Недостаточно прав для выполнения действия');
+        return Promise.reject({ reason: 'Недостаточно прав для выполнения действия', response, status });
+      }
+      case HTTP_REQUEST_STATUS.CONFLICT: {
+        return Promise.reject({ reason: `Пользователь с такими данными уже существует: ${response.reason.split(' ')[0]}`, response, status });
+      }
+      case HTTP_REQUEST_STATUS.FAILED: {
+        return Promise.reject({ reason: 'Ошибка запроса', response, status });
+      }
+      default: {
+        console.log('При выполнении запроса возникла неизвестная ошибка');
+        return Promise.reject({ reason: 'При выполнении запроса возникла неизвестная ошибка', response, status });
+      }
+    }
+  }
+
+  private static getRequestStatus(statusCode: number) {
+    if(statusCode >= 200 && statusCode < 300) return HTTP_REQUEST_STATUS.SUCCESS;
+    if(statusCode === 401) return HTTP_REQUEST_STATUS.UNAUTHORIZED;
+    if(statusCode === 403) return HTTP_REQUEST_STATUS.FORBIDDEN;
+    if(statusCode === 409) return HTTP_REQUEST_STATUS.CONFLICT;
+    return HTTP_REQUEST_STATUS.FAILED;
+  }
+
+  public makeHTTPRequest(
+    url: string,
+    options: THttpRequestOptions,
+  ): makeHTTPRequestResponse {
+    const {
+      method = HTTP_METHOD.GET,
+      data,
+      headers,
+      isFile = false,
+    } = options;
+
+    const urlWithHost = [this.host, url].join('/');
+
+    const urlToRequest = method === HTTP_METHOD.GET ?
+      [urlWithHost, this.queryStringify(data as Record<string, unknown>)].join('') :
+      urlWithHost;
+
+    const fetchParams: RequestInit = method === HTTP_METHOD.GET || !data ?
+      ({
+        method,
+        headers,
+        credentials: 'same-origin',
+      }) :
+      ({
+        method,
+        headers,
+        credentials: 'same-origin',
+        body: isFile ? data as XMLHttpRequestBodyInit : JSON.stringify(data as Record<string, unknown>),
+      })
+
+    return fetch(urlToRequest, fetchParams)
+      .then(response => Promise.all([response.json(), response.status]))
+      .then(([jsonResponse, status]) => ({
+        status: HTTPTransport.getRequestStatus(status),
+        response: jsonResponse,
+      }))
+      .catch(() => ({ status: HTTP_REQUEST_STATUS.CONNECTION_ERROR }));
+  }
+}
