95dec468945f04bdb47cfa7f23f33f4b4b370ccb
natalia127 <ndorogan@bk.ru>
2022-04-26 13:58:44 +0300

    fix edit

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -10,89 +10,68 @@ import {
   GridItem,
   Icon,
 } from "@chakra-ui/react";
-import { VALIDATION } from "constants/validation";
 import { FormikProvider, useFormik } from "formik";
-import { Input } from 'components/Input/Input'
+import { Input } from "components/Input/Input";
 import Upload, { UploadProps } from "rc-upload";
-import {
-  FiEdit
-} from 'react-icons/fi';
-type ProfileProps = Record<string, unknown>;
-const EMPTY_STRING = "";
+import { FiEdit } from "react-icons/fi";
+import { PROFILE_FORM_SCHEMA, INITIAL_STATE } from "./constans";
+import { TProfileProps } from "./types";
 
-const PROFILE_FORM_SCHEMA = [
-  {
-    key: "login",
-    label: "Логин",
-    placeholder: EMPTY_STRING,
-    gridProps: {
-      colSpan: 2,
-    },
-    validate: (value: string) => {
-      if (value.match(VALIDATION.LOGIN.pattern) == null) {
-        return VALIDATION.LOGIN.message;
-      }
-    },
-  },
-  {
-    key: "email",
-    label: "Почта",
-    placeholder: EMPTY_STRING,
-    validate: (value: string) => {
-      if (value.match(VALIDATION.EMAIL.pattern) == null) {
-        return VALIDATION.EMAIL.message;
-      }
-    },
-  },
-  {
-    key: "first_name",
-    label: "Имя",
-    placeholder: EMPTY_STRING,
-    validate: (value: string) => {
-      if (value.match(VALIDATION.FIRST_NAME.pattern) == null) {
-        return VALIDATION.FIRST_NAME.message;
-      }
-    },
-  },
-  {
-    key: "second_name",
-    label: "Фамилия",
-    placeholder: EMPTY_STRING,
-    validate: (value: string) => {
-      if (value.match(VALIDATION.SECOND_NAME.pattern) == null) {
-        return VALIDATION.SECOND_NAME.message;
-      }
-    },
-  },
-  {
-    key: "phone",
-    label: "Телефон",
-    placeholder: EMPTY_STRING,
-    validate: (value: string) => {
-      if (value.match(VALIDATION.PHONE.pattern) == null) {
-        return VALIDATION.PHONE.message;
-      }
-    },
-  },
-];
-
-const INITIAL_STATE = {
-  login: "login",
-  email: "name@yandex.ru",
-  first_name: "Иван",
-  second_name: "Иванов",
-  phone: "89009999999",
+const renderButtons = (
+  isEdit: boolean,
+  toggleEdit: React.MouseEventHandler,
+  isSubmitBtnDisabled: boolean
+) => {
+  if (isEdit) {
+    return (
+      <Flex align="center" justify="center">
+        <Button w="50%" mr={3} onClick={toggleEdit}>
+          Отмена
+        </Button>
+        <Button
+          w="50%"
+          type="submit"
+          colorScheme="purple"
+          isDisabled={isSubmitBtnDisabled}
+        >
+          Сохранить
+        </Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex align="center" justify="flex-end">
+      <Button variant="outline" w="50%" mr={3} onClick={toggleEdit}>
+        Редактировать
+      </Button>
+    </Flex>
+  );
 };
 
-export const Profile: FC<ProfileProps> = () => {
+const renderUpload = (propsUpload: UploadProps, isEdit: boolean) => {
+  if (isEdit) {
+    // принимает src. по этому загрузка файла будет в отдельной ветке
+    return (
+      <Upload
+        {...propsUpload}
+        className="profile__upload"
+        accept="image/jpeg,image/png,image/gif"
+      >
+        <Icon as={FiEdit} w={8} h={8} color="grey" />
+      </Upload>
+    );
+  }
+  return "";
+};
+
+export const Profile: FC<TProfileProps> = () => {
   const [isEdit, setIsEdit] = useState(false);
 
   const onSubmit = useCallback((values) => {
     console.log(values);
   }, []);
 
-
-  const toggleEdit = useCallback(() => setIsEdit(!isEdit), [isEdit]);
+  const toggleEdit = () => setIsEdit(!isEdit);
 
   const formik = useFormik({
     initialValues: INITIAL_STATE,
@@ -106,72 +85,28 @@ export const Profile: FC<ProfileProps> = () => {
       values === INITIAL_STATE || Object.values(errors).some((item) => !!item),
     [values, errors]
   );
-
-
-  const renderButtons = useCallback(() => {
-    if (isEdit) {
-      return (
-        <Flex align="center" justify="center">
-          <Button w="50%" mr={3} onClick={toggleEdit}>
-            Отмена
-          </Button>
-          <Button
-            w="50%"
-            type="submit"
-            colorScheme="purple"
-            isDisabled={isSubmitBtnDisabled}
-          >
-            Сохранить
-          </Button>
-        </Flex>
-      );
-    }
-    return (
-      <Flex align="center" justify="flex-end">
-        <Button variant="outline" w="50%" mr={3} onClick={toggleEdit}>
-          Редактировать
-        </Button>
-      </Flex>
-    );
-  }, [isEdit, isSubmitBtnDisabled, toggleEdit]);
-
-  const renderUpload = useCallback(() => {
-
-    const propsUpload: UploadProps = {
-      action: () => {
-        return new Promise((resolve) => {
-          setTimeout(() => {
-            resolve("/upload.do");
-          }, 2000);
-        });
-      },
-      multiple: true,
-      onStart(file: File) {
-        console.log("onStart", file, file.name);
-      },
-      onSuccess() {
-        console.log("onSuccess");
-      },
-      onError() {
-        console.log("onError");
-      },
-    };
-    if (isEdit) {
-      // принимает src. по этому загрузка файла будет в отдельной ветке
-      return (
-        <Upload {...propsUpload} className="profile__upload" accept="image/jpeg,image/png,image/gif">
-          <Icon as={FiEdit} w={8} h={8} color="grey" />
-        </Upload>
-      );
-    }
-    return ''
-
-  }, [isEdit]);
-
-  let classAvatar = 'profile__avatar' 
-  if (isEdit) {
-    classAvatar+=' profile__avatar--opacity' 
-  }
+  const propsUpload: UploadProps = {
+    action: () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve("/upload.do");
+        }, 2000);
+      });
+    },
+    multiple: true,
+    onStart(file: File) {
+      console.log("onStart", file, file.name);
+    },
+    onSuccess() {
+      console.log("onSuccess");
+    },
+    onError() {
+      console.log("onError");
+    },
+  };
+  const classAvatar = isEdit
+    ? "profile__avatar profile__avatar--opacity"
+    : "profile__avatar";
 
   return (
     <Background>
@@ -180,7 +115,7 @@ export const Profile: FC<ProfileProps> = () => {
           <div className="dot-avatar"></div>
           <Box className="profile__avatar-wrap">
             <Avatar className={classAvatar} bg="red.500" size="lg" />
-            {renderUpload()}
+            {renderUpload(propsUpload, isEdit)}
           </Box>
 
           <div className="dot-avatar"></div>
@@ -193,23 +128,25 @@ export const Profile: FC<ProfileProps> = () => {
                   ({ key, label, placeholder, validate, gridProps = {} }) => {
                     return (
                       <GridItem key={key} {...gridProps}>
-                        {Input({
-                          variant: isEdit ? "outline" : "unstyled",
-                          key,
-                          label,
-                          validate,
-                          placeholder,
-                          error: errors[key as keyof typeof errors],
-                          touched: touched[key as keyof typeof touched],
-                          value: values[key as keyof typeof values],
-                          onChange: handleChange,
-                          isReadOnly: !isEdit,
-                        })}
+                        <Input
+                          variant={isEdit ? "outline" : "unstyled"}
+                          key={key}
+                          label={label}
+                          validate={validate}
+                          placeholder={placeholder}
+                          error={errors[key as keyof typeof errors]}
+                          touched={touched[key as keyof typeof touched]}
+                          value={values[key as keyof typeof values]}
+                          onChange={handleChange}
+                          isReadOnly={!isEdit}
+                        />
                       </GridItem>
                     );
                   }
                 )}
-                <GridItem colStart={2}>{renderButtons()}</GridItem>
+                <GridItem colStart={2}>
+                  {renderButtons(isEdit, toggleEdit, isSubmitBtnDisabled)}
+                </GridItem>
               </Grid>
             </form>
           </FormikProvider>
---
+++ src/pages/Profile/constans.ts
@@ -0,0 +1,66 @@
+import { EMPTY_STRING } from 'constants/generalConst'
+import { VALIDATION } from "constants/validation";
+
+export const PROFILE_FORM_SCHEMA = [
+  {
+    key: "login",
+    label: "Логин",
+    placeholder: EMPTY_STRING,
+    gridProps: {
+      colSpan: 2,
+    },
+    validate: (value: string) => {
+      if (value.match(VALIDATION.LOGIN.pattern) == null) {
+        return VALIDATION.LOGIN.message;
+      }
+    },
+  },
+  {
+    key: "email",
+    label: "Почта",
+    placeholder: EMPTY_STRING,
+    validate: (value: string) => {
+      if (value.match(VALIDATION.EMAIL.pattern) == null) {
+        return VALIDATION.EMAIL.message;
+      }
+    },
+  },
+  {
+    key: "first_name",
+    label: "Имя",
+    placeholder: EMPTY_STRING,
+    validate: (value: string) => {
+      if (value.match(VALIDATION.FIRST_NAME.pattern) == null) {
+        return VALIDATION.FIRST_NAME.message;
+      }
+    },
+  },
+  {
+    key: "second_name",
+    label: "Фамилия",
+    placeholder: EMPTY_STRING,
+    validate: (value: string) => {
+      if (value.match(VALIDATION.SECOND_NAME.pattern) == null) {
+        return VALIDATION.SECOND_NAME.message;
+      }
+    },
+  },
+  {
+    key: "phone",
+    label: "Телефон",
+    placeholder: EMPTY_STRING,
+    validate: (value: string) => {
+      if (value.match(VALIDATION.PHONE.pattern) == null) {
+        return VALIDATION.PHONE.message;
+      }
+    },
+  },
+];
+
+export const INITIAL_STATE = {
+  login: "login",
+  email: "name@yandex.ru",
+  first_name: "Иван",
+  second_name: "Иванов",
+  phone: "89009999999",
+};
