27c7616fb9e0e82b1c24047f1346a021150d0fa8
Leonid Sergeev <sls@bfg.ai>
2022-05-06 18:46:42 +0400

    #44  - проработка логики отображения модального окна присотановки и завершения игрового процесса на осонвании локального стейта компонента и рефа для хранения колбэка приостановки игры

--- src/pages/GamePlay/GamePlay.tsx
+++ src/pages/GamePlay/GamePlay.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useCallback } from 'react';
+import React, {useRef, useEffect, useCallback, useState} from 'react';
 import './style.scss';
 import { Game } from './Game/Game';
 import { useHistory } from 'react-router-dom';
@@ -12,7 +12,16 @@ import _isFunction from 'lodash/isFunction';
 export const GamePlay = () => {
   const refCanvas = useRef(null);
   const refScreen = useRef(null);
+
+  /*
+  *  может показаться, что здесь реф для хранения колбэка лишний, но по факту оказывается, что
+  * если использовать стейт для хранения колбэка снятия игры с паузы, столкнемся с проблемой, что при
+  * установке на паузу модальник не открывается а скорость игры увеличивается, т.к. постоянно перерисовывается
+  * компонент канваса из-за обновления локального стейта компонента. Вероятно можно использовать какой-то постоянный кей
+  * чтобы отключить возможность обновления компонента канваса при обновлшении локального стейта компонента
+  * */
   const unpauseCbRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   const history = useHistory();
   const goToGameOverScreen = useCallback(
@@ -47,6 +56,7 @@ export const GamePlay = () => {
           onGamePause: unpauseCb => {
             // @ts-ignore
             unpauseCbRef.current = unpauseCb;
+            setIsOpen(true);
           },
         });
 
@@ -57,13 +67,19 @@ export const GamePlay = () => {
 
   return (
     <div ref={refScreen} className="playing-field">
-      <Modal isOpen onClose={() => { console.log('onCLose');}}>
+      <Modal isOpen={isOpen} onClose={() => { console.log('onCLose');}}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Пауза</ModalHeader>
           <ModalBody>
-            {/*@ts-ignore*/}
-            <Button onClick={() => _isFunction(unpauseCbRef.current) && unpauseCbRef.current()}>
+            <Button
+              mr={2}
+              onClick={() => {
+                /*@ts-ignore*/
+                _isFunction(unpauseCbRef.current) && unpauseCbRef.current();
+                setIsOpen(false);
+              }}
+            >
               Продолжить игру
             </Button>
             <Button onClick={goToGameOverScreen}>
@@ -72,7 +88,7 @@ export const GamePlay = () => {
           </ModalBody>
         </ModalContent>
       </Modal>
-      <canvas ref={refCanvas} width={CANVAS_SIZE_IN_PX} height={CANVAS_SIZE_IN_PX} />
+      <canvas key={111} ref={refCanvas} width={CANVAS_SIZE_IN_PX} height={CANVAS_SIZE_IN_PX} />
     </div>
   );
 };
