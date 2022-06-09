51d8468071ff251a0419d864c2327e328238b743
Leonid Sergeev <sls@bfg.ai>
2022-06-09 23:20:25 +0400

    - подключен драйвер postgresql - настроен запуск для локальной разработки - корректировки конфигурации

--- index.js
+++ index.js
@@ -1,32 +1,58 @@
 const { MongoClient } = require('mongodb');
 const { secureServer } = require('./dist/server.js');
+const { Client } = require('pg');
+
 require('dotenv').config();
 
 
-const port = process.env.PORT || 5000;
+const connectToMongoDb = () => {
+  const url = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
+  const client = new MongoClient(url);
 
-const url = `mongodb://${process.env.MONGO_HOST}:27017`;
-
-const dbName = process.env.MONGO_DB_NAME;
-const client = new MongoClient(url);
-
-client
-  .connect()
-  .then(() => {
-
-    const db = client.db(dbName);
-    console.log('mongo db is ready: ' + db)
-
-    secureServer.listen(port, () => {
-      console.log(
-        'Application is started on https://local.ya-praktikum.tech',
-        port,
-      );
+  return client
+    .connect()
+    .then(() => console.log('соединение с mongoDB установлено'))
+    .catch(error => {
+      console.log('не удалось установить соединение с mongoDB');
+      throw error;
     });
-  })
-  .catch(error => {
-    console.log('mongo connection lost');
-    throw error;
-  })
+};
+
+const connectToPostgreSQL = () => {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRESQL_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRESQL_PORT,
+  });
+
+  return client
+    .connect()
+    .then(() => console.log('соединение с postgreSQL установлено'))
+    .catch(error => {
+      console.log('не удалось установить соединение с postgreSQL');
+      throw error;
+    });
+};
+
+const launchExpressServer = () => {
+  const port = process.env.PORT || 5000;
+
+  secureServer.listen(
+    port,
+    () => console.log(`Приложение запущено по адресу: https://local.ya-praktikum.tech:${port}`));
+};
+
+const handleCommonError = error => {
+  console.log('В процессе запуска приложения возникла критическая ошибка');
+};
+
+Promise.resolve()
+  .then(connectToMongoDb)
+  .then(connectToPostgreSQL)
+  .then(launchExpressServer)
+  .catch(handleCommonError);
+
 
 
