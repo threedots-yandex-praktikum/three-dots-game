9924deffc53adc3ba35e17fdcebfded0d6ae72ef
Leonid Sergeev <sls@bfg.ai>
2022-06-09 21:45:26 +0400

    - завершение настройки compose - добавлен .env-example

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const { MongoClient } = require('mongodb');
 const { secureServer } = require('./dist/server.js');
-
 require('dotenv').config();
 
-console.log('mongo host', process.env.MONGO_HOST);
-console.log('mongo password', process.env.MONGO_PASSWORD);
 
 const port = process.env.PORT || 5000;
 
 const url = `mongodb://${process.env.MONGO_HOST}:27017`;
 
-const dbName = 'test-db';
+const dbName = process.env.MONGO_DB_NAME;
 const client = new MongoClient(url);
 
 client
