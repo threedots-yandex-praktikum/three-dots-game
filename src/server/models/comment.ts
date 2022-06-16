3d82b4fc074e0b0196e719a93c8166e4ec036ad9
natalia127 <ndorogan@bk.ru>
2022-06-16 19:45:22 +0300

    fix

--- src/server/models/comment.ts
+++ src/server/models/comment.ts
@@ -46,7 +46,7 @@ export class Comment extends Model<CommentAttributes, CommentCreationAttributes>
   @Column({
     type: DataType.INTEGER,
     allowNull: false,
-    onDelete: 'cascade',
+    onDelete: 'CASCADE',
   })
   topicId!: number;
 
