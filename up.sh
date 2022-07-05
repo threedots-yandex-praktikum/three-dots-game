9acf0f7d569ee7ccd9fcb6f769d77125c5fef9d5
Leonid Sergeev <sls@bfg.ai>
2022-07-05 22:57:22 +0400

    up script

---
+++ up.sh
@@ -0,0 +1,13 @@
+!bin/bash
+
+# переходим в директорию с docker-compose.yml
+cd /home/deploy/three-dots-game/
+
+# останавливаем и удаляем все существующие контейнеры
+docker-compose stop
+docker-compose rm -f
+
+# запускаем все необходимые контейнеры
+docker-compose up
+
+
