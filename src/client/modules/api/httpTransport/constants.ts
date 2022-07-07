dad5b4d6c8806f47d407b0527340781b6270c928
Leonid Sergeev <sls@bfg.ai>
2022-07-07 22:59:24 +0400

    корректировки после мерджа

--- src/client/modules/api/httpTransport/constants.ts
+++ src/client/modules/api/httpTransport/constants.ts
@@ -1,12 +1,12 @@
 export const YANDEX_API_HOST = 'https://ya-praktikum.tech/api/v2';
 export const YANDEX_OAUTH_HOST = 'https://oauth.yandex.ru';
-export const LOCAL_API_HOST = 'https://localhost:5000/api';
+export const LOCAL_API_HOST = 'http://localhost:5000/api';
 
 export const TEAM_NAME = 'three-dots';
 
 export const RATING_FIELD_NAME = 'score';
 
-export const BACKEND_PATH = 'https://localhost:5000';
+export const BACKEND_PATH = 'http://localhost:5000';
 
 export const AUTH_API_ENDPOINTS = {
   SIGN_UP: 'auth/signup',
