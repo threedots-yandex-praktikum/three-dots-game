66e04920ffae9746f00abcae650bb852cdad768b
natalia127 <ndorogan@bk.ru>
2022-07-07 23:00:23 +0300

    изменила LOCAL_API_HOST и BACKEND_PATH

--- src/client/modules/api/httpTransport/constants.ts
+++ src/client/modules/api/httpTransport/constants.ts
@@ -1,12 +1,12 @@
 export const YANDEX_API_HOST = 'https://ya-praktikum.tech/api/v2';
 export const YANDEX_OAUTH_HOST = 'https://oauth.yandex.ru';
-export const LOCAL_API_HOST = `https://${process.env.HOST}/api`;
+export const LOCAL_API_HOST = `/api`;
 
 export const TEAM_NAME = 'three-dots';
 
 export const RATING_FIELD_NAME = 'score';
 
-export const BACKEND_PATH = process.env.HOST;
+export const BACKEND_PATH = '';
 
 export const AUTH_API_ENDPOINTS = {
   SIGN_UP: 'auth/signup',
