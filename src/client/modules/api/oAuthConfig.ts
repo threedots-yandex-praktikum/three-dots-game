af069e0cae694c7fc0124844b0f507d19712aceb
Leonid Sergeev <sls@bfg.ai>
2022-07-10 12:27:09 +0400

    oauth

--- src/client/modules/api/oAuthConfig.ts
+++ src/client/modules/api/oAuthConfig.ts
@@ -1,4 +1,4 @@
 import {YANDEX_CLOUD_HOST, YANDEX_OAUTH_HOST} from "client/modules/api/httpTransport/constants";
 
 export const getUrlOauth = (clientId:string, redirectUrl = YANDEX_CLOUD_HOST) =>
-`https://${YANDEX_OAUTH_HOST}/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUrl}`;
+`${YANDEX_OAUTH_HOST}/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUrl}`;
